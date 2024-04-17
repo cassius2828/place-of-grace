4eb888c3cec33b1da20305ef57a3e17324729182
Cassius Reynolds <cassius.reynolds.dev@gmail.com>
2024-04-17 02:34:39 -0700

    added new photos to serve page

--- src/components/ImageFloats/ImageFloats.jsx
+++ src/components/ImageFloats/ImageFloats.jsx
@@ -1,27 +1,47 @@
-import picnic from "/images/clients/home2.png";
-import party from "/images/clients/home1.jpg";
+import party from "/images/clients/home2.png";
+import picnic from "/images/clients/home1.jpg";
 import xmas from "/images/clients/home3.png";
+import ParkBench from "/images/clients/Park-Bench-Group.jpeg";
+import WaterFight from "/images/clients/Rome-Javon-Waterfight.jpeg";
+import CarGroup from "/images/clients/Car-Group.jpeg";
+import EstherGroup from "/images/clients/Esther-Group-House.jpeg";
+// import groupLeft from "/images/clients/group-left.png";
+// import groupMiddle from "/images/clients/group-middle.png";
+// import groupRight from "/images/clients/group-right.png";
+import groupwide from "/images/clients/group-wide.jpeg";
 
 const ImageFloats = () => {
   return (
     <>
       <div className="composition">
         <img
-          src={picnic}
+          src={EstherGroup}
           alt="Photo 1"
           className="composition__photo composition__photo--p1"
           loading="lazy"
         />
         <img
-          src={party}
+          src={ParkBench}
           alt="Photo 2"
           className="composition__photo composition__photo--p2"
           loading="lazy"
         />
         <img
-          src={xmas}
+          src={WaterFight}
           alt="Photo 3"
           className="composition__photo composition__photo--p3"
+          loading="lazy"
+        />
+            <img
+          src={party}
+          alt="Photo 4"
+          className="composition__photo composition__photo--p4"
+          loading="lazy"
+        />
+             <img
+          src={picnic}
+          alt="Photo 5"
+          className="composition__photo composition__photo--p5"
           loading="lazy"
         />
       </div>{" "}
--- src/components/Reusables/Carousel.jsx
+++ src/components/Reusables/Carousel.jsx
@@ -1,13 +1,17 @@
-import picnic from "/images/clients/home2.png";
-import party from "/images/clients/home1.jpg";
+import party from "/images/clients/home2.png";
+import picnic from "/images/clients/home1.jpg";
 import xmas from "/images/clients/home3.png";
+import ParkBench from "/images/clients/Park-Bench-Group.jpeg";
+import WaterFight from "/images/clients/Rome-Javon-Waterfight.jpeg";
+import CarGroup from "/images/clients/Car-Group.jpeg";
+import EstherGroup from "/images/clients/Esther-Group-House.jpeg";
 // import groupLeft from "/images/clients/group-left.png";
 // import groupMiddle from "/images/clients/group-middle.png";
 // import groupRight from "/images/clients/group-right.png";
 import groupwide from "/images/clients/group-wide.jpeg";
 
 import Slider from "react-slick";
-const Carousel = ({slidesToShow}) => {
+const Carousel = ({ slidesToShow }) => {
   var settings = {
     dots: true,
     infinite: true,
@@ -18,24 +22,35 @@ const Carousel = ({slidesToShow}) => {
     fade: true,
     autoplay: true,
     autoplaySpeed: 5000,
-   
   };
   return (
     <Slider {...settings}>
       <div className="carousel-img-container">
+        <img
+          src={EstherGroup}
+          alt=""
+          loading="lazy"
+        />
+      </div>
+      <div className="carousel-img-container">
+        <img
+          src={ParkBench}
+          alt=""
+          loading="lazy"
+        />
+      </div>
+      <div className="carousel-img-container">
+        <img
+          src={WaterFight}
+          alt=""
+          loading="lazy"
+        />
+      </div>
+      <div className="carousel-img-container">
+        <img src={party} alt="" />
+      </div>
+      <div className="carousel-img-container">
         <img src={picnic} alt="" loading="lazy" />
-      </div>
-      <div className="carousel-img-container">
-        <img src={party} alt="" loading="lazy" />
-      </div>
-      <div className="carousel-img-container">
-        <img src={xmas} alt="" loading="lazy" />
-      </div>
-      {/* <div className="carousel-img-container">
-        <img src={groupLeft} alt="" />
-      </div> */}
-      <div className="carousel-img-container">
-        <img src={groupwide} alt="" loading="lazy" />
       </div>
       {/* <div className="carousel-img-container">
         <img src={groupRight} alt="" />
