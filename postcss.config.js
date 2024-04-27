662de1f91003a2c443753ba0ca47af394de2005b
Cassius Reynolds <cassius.reynolds.dev@gmail.com>
2024-04-27 14:00:58 -0700

    made patch and minor updates to all packages. Eslint is staying at 8 to prevent breaking changes that occur in 9

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
 import autoprefixer from "autoprefixer";
-// import purgecss from "@fullhuman/postcss-purgecss";
+
 export default {
   plugins: [
     autoprefixer(),
-    // purgecss({
-    //   content: ["./dist/**/*.html", "./dist/**/*.{js,jsx}"], // Adjust this according to your project structure
-    //   // Other PurgeCSS options...
-    // }),
   ],
 };
