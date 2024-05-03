f004c4b8986fd74028441c4a6de7e03a5ad22480
Cassius Reynolds <cassius.reynolds.dev@gmail.com>
2024-05-03 00:14:46 -0700

    removed fallback image to see how much it impacts performace rating

--- src/components/BgVideo/BgVideo.jsx
+++ src/components/BgVideo/BgVideo.jsx
@@ -1,7 +1,35 @@
 import VideoOfHills from "/videos/formHillsBackground.mp4";
 import VideoOfHillsWebm from "/videos/formHillsBackground.webm";
-import fallbackHills from "/images/wide-images/hillsArielImage.png";
+// import fallbackHills from "/images/wide-images/hillsArielImage.png";
 import { useState } from "react";
+
+// const BgVideo = () => {
+//   const [isVideoError, setIsVideoError] = useState(false);
+//   const handleError = () => {
+//     setIsVideoError(true);
+//   };
+//   return (
+//     <div className="bg-video">
+//       {isVideoError ? (
+//         <img src={fallbackHills} alt="Fallback image of soaring over hills" />
+//       ) : (
+//         <video
+//           onError={handleError}
+//           className="bg-video__content"
+//           loading="lazy"
+//           autoPlay
+//           muted
+//           loop
+//           playsInline
+//         >
+//           <source src={VideoOfHills} type="video/mp4" />
+//           <source src={VideoOfHillsWebm} type="video/webm" />
+//         </video>
+//       )}
+//     </div>
+//   );
+// };
+// export default BgVideo;
 
 const BgVideo = () => {
   const [isVideoError, setIsVideoError] = useState(false);
@@ -10,22 +38,18 @@ const BgVideo = () => {
   };
   return (
     <div className="bg-video">
-      {isVideoError ? (
-        <img src={fallbackHills} alt="Fallback image of soaring over hills" />
-      ) : (
-        <video
-          onError={handleError}
-          className="bg-video__content"
-          loading="lazy"
-          autoPlay
-          muted
-          loop
-          playsInline
-        >
-          <source src={VideoOfHills} type="video/mp4" />
-          <source src={VideoOfHillsWebm} type="video/webm" />
-        </video>
-      )}
+      <video
+        onError={handleError}
+        className="bg-video__content"
+        loading="lazy"
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source src={VideoOfHills} type="video/mp4" />
+        <source src={VideoOfHillsWebm} type="video/webm" />
+      </video>
     </div>
   );
 };
